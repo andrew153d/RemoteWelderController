a2ce661559f3a95fba0f5c029e7536a63294e052
andrew153d <andrewwisnergrow@gmail.com>
2024-04-10 20:09:06 -0400

    update build script to include css and js

--- firmware/scripts/assemble_website.py
+++ firmware/scripts/assemble_website.py
@@ -4,7 +4,7 @@ from collections import deque
 import base64
 import os
 
-
+original_dir = os.getcwd()
 
 def image_to_base64(image_path):
     with open(image_path, "rb") as image_file:
@@ -27,6 +27,16 @@ def get_img_positions(html_string):
         index_queue.append(quot_index)
     return index_queue
 
+def get_html_from_file(html_file_path):
+    with open(html_file_path, 'r') as html_file:
+        html_content = html_file.read()
+    return html_content
+
+def insert_css_into_html(key, html_string ,file_path):
+    with open(file_path, 'r') as file:
+        css_content = file.read()
+        return html_string.replace(key, css_content)
+
 def replace_img_src(html_string):
     
     positions = get_img_positions(html_string)
@@ -44,19 +54,13 @@ def replace_img_src(html_string):
 
     return modified_html
 
-
-
-def insert_html_into_header(html_file_path, header_file):
+def insert_javascript_into_html(key, html_content ,js_path):
+    with open(js_path, 'r') as file:
+        js_content = file.read()
+        html_content = html_content.replace(key, '<script>'+js_content+'</script>')
+        return html_content
     
-    original_dir = os.getcwd()
-
-    with open(html_file_path, 'r') as html_file:
-        html_content = html_file.read()
-    dird = os.path.split(os.path.abspath(html_file_path))[0]
-    print(dird)
-    os.chdir(dird)
-
-    html_content = replace_img_src(html_content)
+def convert_html_string_to_header(html_content):
 
     html_content = html_content.replace('"', '\\"')
     # Replace newlines with escaped newlines and add quotation marks to each line
@@ -69,11 +73,29 @@ const char *web_html = \"{}\"
                       \"\";
 
 #endif""".format(html_content)
+    return header_content
 
-    # Write the combined content into the header file
+def write_html_to_html_file(html_string, html_path):
+    with open(html_path, 'w') as file:
+        file.write(html_string)
+
+def write_html_formatted_string_to_header(html_content, file_path):
+    global original_dir
     os.chdir(original_dir)
-    with open(header_file, 'w') as header_file:
-        header_file.write(header_content)
+    with open(file_path, 'w') as file:
+        file.write(html_content)
 
+html_file_path = "firmware\webpage\web_controller.html"
 
-insert_html_into_header("firmware\webpage\web_controller.html", 'firmware\include\web_controller.h')
+html_page = get_html_from_file(html_file_path)
+html_page = insert_css_into_html("this_gets_replaced{width: 80px;}",html_page,"firmware\webpage\styles.css")
+original_dir = os.getcwd()
+dird = os.path.split(os.path.abspath(html_file_path))[0]
+print(dird)
+os.chdir(dird)
+html_page  = replace_img_src(html_page)
+os.chdir(original_dir)
+html_page=insert_javascript_into_html('<script src="scripts.js"></script>', html_page, "firmware/webpage/scripts.js")
+write_html_to_html_file(html_page, "firmware\webpage\generated.html")
+html_page = convert_html_string_to_header(html_page)
+write_html_formatted_string_to_header(html_page, 'firmware\include\web_controller.h')
---
+++ firmware/webpage/scripts.js
@@ -0,0 +1,138 @@
+const circle = document.getElementById("control_knob");
+      const slider = document.getElementById("slide-container");
+      let isMouseDown = false;
+      let prevAngle = 0;
+      let currentRotation = 0;
+      let menuOpen = false;
+
+      function menuPressed() {
+        console.log("opening");
+        var menuItems = document.querySelectorAll(".menu_item");
+        menuItems.forEach(function (menuItem) {
+          menuItem.classList.toggle("open");
+
+          document.getElementById("menu").classList.toggle(".x_menu");
+
+
+        });
+      }
+
+      // Function to calculate angle between two points
+      function getAngle(center, point) {
+        console.log(
+          "%d, %d, %d, %d",
+          point.clientY,
+          center.top,
+          point.clientX,
+          center.left
+        );
+        const deltaY = point.clientY - center.top;
+        const deltaX = point.clientX - center.left;
+        const angle = Math.atan2(deltaY, deltaX);
+        console.log("%f, %f, %f", deltaY, deltaX, angle);
+        return angle;
+      }
+
+      // Function to rotate the circle
+      function rotateCircle(event) {
+        event.preventDefault();
+        let touch = event;
+        if (event.touches) {
+          touch = event.touches[0];
+        }
+        if (isMouseDown) {
+          const rect = circle.getBoundingClientRect();
+          const center = {
+            left: rect.left + rect.width / 2,
+            top: rect.top + rect.height / 2,
+          };
+
+          const angle = getAngle(center, touch);
+          const rotation = angle - prevAngle;
+          //console.log(rotation);
+          currentRotation += rotation;
+          circle.style.transform = `rotate(${currentRotation}rad)`;
+          prevAngle = angle;
+          //console.log(currentRotation);
+        }
+      }
+
+      // Event listeners for mouse down, move, and up
+      circle.addEventListener(
+        "mousedown",
+        (event) => {
+          isMouseDown = true;
+          const rect = circle.getBoundingClientRect();
+          const center = {
+            left: rect.left + rect.width / 2,
+            top: rect.top + rect.height / 2,
+          };
+          prevAngle = getAngle(center, event);
+        },
+        { passive: false }
+      );
+
+      slider.addEventListener(
+        "mousedown",
+        (event) => {
+          isMouseDown = true;
+          const rect = circle.getBoundingClientRect();
+          const center = {
+            left: rect.left + rect.width / 2,
+            top: rect.top + rect.height / 2,
+          };
+          prevAngle = getAngle(center, event);
+        },
+        { passive: false }
+      );
+
+      document.addEventListener("mousemove", rotateCircle, { passive: false });
+
+      document.addEventListener(
+        "mouseup",
+        () => {
+          isMouseDown = false;
+        },
+        { passive: false }
+      );
+
+      // Event listeners for touch events
+      circle.addEventListener(
+        "touchstart",
+        (event) => {
+          isMouseDown = true;
+          const touch = event.touches[0];
+          const rect = circle.getBoundingClientRect();
+          const center = {
+            left: rect.left + rect.width / 2,
+            top: rect.top + rect.height / 2,
+          };
+          prevAngle = getAngle(center, touch);
+        },
+        { passive: false }
+      );
+
+      slider.addEventListener(
+        "touchstart",
+        (event) => {
+          isMouseDown = true;
+          const touch = event.touches[0];
+          const rect = circle.getBoundingClientRect();
+          const center = {
+            left: rect.left + rect.width / 2,
+            top: rect.top + rect.height / 2,
+          };
+          prevAngle = getAngle(center, touch);
+        },
+        { passive: false }
+      );
+
+      document.addEventListener("touchmove", rotateCircle, { passive: false });
+
+      document.addEventListener(
+        "touchend",
+        () => {
+          isMouseDown = false;
+        },
+        { passive: false }
+      );
